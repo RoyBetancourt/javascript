017c346b8c21ca5df456bcda48fe8616077fa4d0
RoyBetancourt <betancourt.roy17@csj-rpi.org>
2015-09-17 09:34:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function changeImage() {
   var image = document.getElementById('myImage');
   if (image.src.match("bulon")){
-    image.src="image/pic_bulboff.gif"
+    image.src="images/pic_bulboff.gif"
   } else {
     image.src = "image/pic_bulbon.gof";
   }
